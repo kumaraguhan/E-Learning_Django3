8f306afee5732a53b5858214a4b913459e35b81b
Guhan <guhan3697figmentmajesty@gmail.com>
2025-07-24 05:48:52 +0000

    Create List and Detail view using REST's generic views
    Here I insatlled restframework module and created some classes for serialising Subject, Course and Module models and IMplemeted nested serialisers for Module inside Course

--- educa/settings.py
+++ educa/settings.py
@@ -30,6 +30,7 @@ INSTALLED_APPS = [
     'embed_video',
     'memcache_status',
     'debug_toolbar',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -136,3 +137,9 @@ CACHES = {
 CACHE_MIDDLEWARE_ALIAS = 'default'
 CACHE_MIDDLEWARE_SECONDS = 60 * 15  # 15 minutes
 CACHE_MIDDLEWARE_KEY_PREFIX = 'educa'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+      'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
