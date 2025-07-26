e023bef94cf4972d790d8d6df8e1533174d2656b
Guhan <guhan3697figmentmajesty@gmail.com>
2025-07-26 05:30:58 +0000

    Deploy 1

--- educa/settings.py
+++ educa/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-tv3h8k55#z+op*flw*fkk6jm8xtb=j66k%6q&plrd9n#=&is@f'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -126,6 +126,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
